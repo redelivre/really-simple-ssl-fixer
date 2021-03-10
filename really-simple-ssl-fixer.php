f8ef74f78c9f0306be8ac2a558afa0fb2525c7c7
Jacson Passold <jacson@passold.com.br>
2021-03-10 11:32:32 -0300

    recreate init

--- really-simple-ssl-fixer.php
+++ really-simple-ssl-fixer.php
@@ -17,14 +17,17 @@ if(defined('rsssl_plugin')) {
 	class RSSSLF {
 		public function __construct() {
 			if (!is_admin() && is_ssl() && RSSSL()->rsssl_front_end->autoreplace_insecure_links) {
-				add_action('init', array($this, 'init'), 11);
+				add_action('init', array($this, 'sslinit'), 11);
 			}
+			add_action('init', array($this, 'init'), 11);
 		}
-		public function init() {
+		public function sslinit() {
 			//add_filter('template_directory_uri', array($this,'fix_template_directory_uri'), 10, 3);
 			add_filter('theme_root_uri', array($this, 'fix_theme_root_uri'), 10, 3);
 			add_filter('includes_url', array($this, 'fix_uri'), 10, 1);
 			add_filter('wp_calculate_image_srcset', array($this, 'wp_calculate_image_srcset'), 10, 1);
+		}
+		public function init() {
 			add_filter('get_user_option_use_ssl', array($this, 'get_user_option_use_ssl') );
 			add_filter('login_redirect', array($this, 'login_redirect'), 99 );
 		}
