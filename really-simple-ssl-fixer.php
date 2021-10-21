f4b9246318c3eebaba83fc55c63cb95c535b790a
Jacson Passold <jacson@passold.com.br>
2021-10-21 02:58:02 -0300

    add fix to divi font url

--- really-simple-ssl-fixer.php
+++ really-simple-ssl-fixer.php
@@ -15,9 +15,6 @@ defined('ABSPATH') or die("you do not have access to this page!");
 
 class RSSSLF {
 	public function __construct() {
-		if (!is_admin() && is_ssl() && defined('rsssl_plugin') && RSSSL()->rsssl_front_end->autoreplace_insecure_links) {
-			add_action('init', array($this, 'sslinit'), 11);
-		}
 		add_action('init', array($this, 'init'), 11);
 	}
 	public function sslinit() {
@@ -25,10 +22,15 @@ class RSSSLF {
 		add_filter('theme_root_uri', array($this, 'fix_theme_root_uri'), 10, 3);
 		add_filter('includes_url', array($this, 'fix_uri'), 10, 1);
 		add_filter('wp_calculate_image_srcset', array($this, 'wp_calculate_image_srcset'), 10, 1);
+		add_filter('et_builder_custom_fonts', array($this, 'et_builder_custom_fonts'), 9, 1 );
 	}
 	public function init() {
 		add_filter('get_user_option_use_ssl', array($this, 'get_user_option_use_ssl') );
 		add_filter('login_redirect', array($this, 'login_redirect'), 99 );
+		if (!is_admin() && is_ssl() && defined('rsssl_plugin') && RSSSL()->rsssl_front_end->autoreplace_insecure_links) {
+			$this->sslinit();
+		}
+		
 	}
 	public function fix_uri($uri) {
 		return preg_replace('/^https?\:/i', '//', $uri); // replace "http://" or "https://" by "//"
@@ -44,7 +46,7 @@ class RSSSLF {
 		return $sources;
 	}
 	public function get_user_option_use_ssl($ssl) {
-		if( isset($_SERVER['HTTPS']) && 'on' == $_SERVER['HTTPS'] ) { //force ssl when enabled 
+		if( isset($_SERVER['HTTPS']) && 'on' == $_SERVER['HTTPS'] ) { //force ssl when enabled
 			return true;
 		}
 		return $ssl;
@@ -55,5 +57,19 @@ class RSSSLF {
 		}
 		return $url;
 	}
+	
+	public function et_builder_custom_fonts($all_custom_fonts) {
+		if(is_array($all_custom_fonts)) {
+			foreach ($all_custom_fonts as $font_name => $font_array) {
+				if(array_key_exists('font_url', $font_array) && is_array($font_array['font_url'])) {
+					foreach ($font_array['font_url'] as $ext => $url) {
+						$all_custom_fonts[$font_name]['font_url'][$ext] = str_replace("http://", "https://", $url);
+					}
+				}
+			}
+		}
+		return $all_custom_fonts;
+	}
+	
 }
 new RSSSLF();
